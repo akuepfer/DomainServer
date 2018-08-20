9500a6c139bf01ed4a4d7ec2bd5b27ab52ea36be
akuepfer <armin.kuepfer@bluewin.ch>
2018-08-20 19:31:06 +0000

    server startup

--- bin/start_server.sh
+++ bin/start_server.sh
@@ -6,12 +6,22 @@
 #  -Dserver.truststore=/etc/letsencrypt/live/armin-kuepfer.com/armin-kuepfer.jks \
 #  -Dserver.truststore.password=$1 \
 
+JAVA_HOME=/usr/local/jdk-10.0.2
+TARGET=/home/armin/ws/DomainServer/target
+LOGDIR=/home/armin/ws/DomainServer/log
 
-java \
+
+find /home/armin/ws/MoveMountains/app -name '*.undertow.encoding.gzip' -exec rm {} \;
+
+
+cd /home/armin/ws/DomainServer/root
+
+nohup java \
   -Dserver.keystore=/etc/letsencrypt/live/armin-kuepfer.com/armin-kuepfer.jks \
   -Dserver.keystore.password=$1 \
-  -Dserver.truststore=/usr/lib/jvm/java-1.8.0-openjdk-1.8.0.181-3.b13.el7_5.x86_64/jre/lib/security/cacerts \
+  -Dserver.truststore=$JAVA_HOME/lib/security/cacerts \
   -Dserver.truststore.password=changeit\
   -Dkey.password=$1 \
-  -cp DomainServer-1.0-SNAPSHOT.jar:./dependency/* \
-  org.kuepfer.DomainServer
+  -cp $TARGET/DomainServer-1.0-SNAPSHOT.jar:$TARGET/dependency/* \
+  org.kuepfer.DomainServer 2>&1 > $LOGDIR/domain-server.log &
+
